98e2b8f264c3326b0bdafc8880e63539ac971234
Alex Freitag <devfreitag@gmail.com>
2023-04-28 17:46:24 -0300

    feat: implements client/bidirectional streaming services

--- src/main/java/com/devfreitag/TodoServiceImpl.java
+++ src/main/java/com/devfreitag/TodoServiceImpl.java
@@ -2,17 +2,75 @@ package com.devfreitag;
 
 import io.grpc.stub.StreamObserver;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class TodoServiceImpl extends TodoServiceGrpc.TodoServiceImplBase {
 
     @Override
     public void createTodo(CreateTodoRequest createTodoRequest, StreamObserver<Todo> todoStreamObserver) {
         Todo todo = Todo.newBuilder()
-                .setId("1")
+                .setId(String.valueOf(UUID.randomUUID()))
                 .setDescription(createTodoRequest.getDescription())
                 .setDone(false)
                 .build();
 
         todoStreamObserver.onNext(todo);
         todoStreamObserver.onCompleted();
+    }
+
+    @Override
+    public StreamObserver<CreateTodoRequest> createTodoClientStreaming(StreamObserver<TodoList> responseObserver) {
+        ArrayList<Todo> todoList = new ArrayList<>();
+
+        return new StreamObserver<CreateTodoRequest>() {
+            @Override
+            public void onNext(CreateTodoRequest createTodoRequest) {
+                Todo todo = Todo.newBuilder()
+                        .setId(String.valueOf(UUID.randomUUID()))
+                        .setDescription(createTodoRequest.getDescription())
+                        .setDone(false)
+                        .build();
+                todoList.add(todo);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                responseObserver.onError(t);
+            }
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onNext(TodoList.newBuilder().addAllTodos(todoList).build());
+                responseObserver.onCompleted();
+            }
+        };
+    }
+
+    @Override
+    public StreamObserver<CreateTodoRequest> createTodoBidirectionalStreaming(StreamObserver<Todo> responseObserver) {
+        return new StreamObserver<CreateTodoRequest>() {
+            @Override
+            public void onNext(CreateTodoRequest createTodoRequest) {
+
+                Todo todo = Todo.newBuilder()
+                        .setId(String.valueOf(UUID.randomUUID()))
+                        .setDescription(createTodoRequest.getDescription())
+                        .setDone(false)
+                        .build();
+                responseObserver.onNext(todo);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                responseObserver.onError(t);
+            }
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onCompleted();
+            }
+
+        };
     }
 }
